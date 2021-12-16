a459eddbdf7ee4d8300d8a0de2794274e172c651
Álaro Lemos <alemos@junglescout.com>
2021-12-15 22:57:32 -0300

    Add tests to PreProd stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -29,20 +29,30 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     });
 
     // This is where we add the application stages
-    pipeline.addStage(
-      new CdkpipelinesDemoStage(this, "PreProd", {
-        env: { account: "676887617081", region: "us-east-1" },
-      })
-    );
+    const preprod = new CdkpipelinesDemoStage(this, "PreProd", {
+      env: { account: "676887617081", region: "us-east-1" },
+    });
+    pipeline.addStage(preprod, {
+      post: [
+        new ShellStep("TestService", {
+          commands: [
+            // Use 'curl' to GET the given URL and fail if it returns an error
+            "curl -Ssf $ENDPOINT_URL",
+          ],
+          envFromCfnOutputs: {
+            // Get the stack Output from the Stage and make it available in
+            // the shell script as $ENDPOINT_URL.
+            ENDPOINT_URL: preprod.urlOutput,
+          },
+        }),
+      ],
+    });
 
-    pipeline.addStage(
-      new CdkpipelinesDemoStage(this, "Prod", {
-        env: { account: "676887617081", region: "us-east-1" },
-      }),
-      {
-        pre: [new ManualApprovalStep("PromoteToProd")],
-      }
-    );
-
+    const prod = new CdkpipelinesDemoStage(this, "Prod", {
+      env: { account: "676887617081", region: "us-east-1" },
+    });
+    pipeline.addStage(prod, {
+      pre: [new ManualApprovalStep("PromoteToProd")],
+    });
   }
 }
