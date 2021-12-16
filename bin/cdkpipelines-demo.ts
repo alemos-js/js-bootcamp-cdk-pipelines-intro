890fee34a692e33c8d24e49e35769940c5168c7e
Álaro Lemos <alemos@junglescout.com>
2021-12-15 21:54:40 -0300

    Added AWS account info

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -5,7 +5,7 @@ import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
-  env: { account: "075520818437", region: "us-east-1" },
+  env: { account: "676887617081", region: "us-east-1" },
 });
 
 app.synth();
